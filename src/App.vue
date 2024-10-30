eafe44a7d9019f39302e8aa9de34070ad547fddf
Reece <reece.cares@gmail.com>
2024-10-30 14:52:08 +1100

    Created component for project reel

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,17 @@
 <script setup>
-//import HelloWorld from './components/HelloWorld.vue'
+import ProjectReel from './components/ProjectReel.vue'
 </script>
 
 <template>
   <header>
-
     <div class="wrapper">
       <!-- <HelloWorld msg="You did it!" /> -->
     </div>
   </header>
 
   <main>
-    <!-- <div style="width:50%"> -->
-    <div style="max-width:675px; margin: auto;">  <!-- images are restricted to this size -->
-
-      <v-carousel cycle="true"> <!-- height is on here -->
-        <v-carousel-item
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          cover
-        ></v-carousel-item>
-
-        <v-carousel-item
-          src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
-          cover
-        ></v-carousel-item>
-
-        <v-carousel-item
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-          cover
-        ></v-carousel-item>
-      </v-carousel>
-
-      <v-carousel cycle="true"> <!-- height is on here -->
-        <v-carousel-item
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          cover
-        ></v-carousel-item>
-
-        <v-carousel-item
-          src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
-          cover
-        ></v-carousel-item>
-
-        <v-carousel-item
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-          cover
-        ></v-carousel-item>
-      </v-carousel>
+     <div style="min-width: 500px; max-width:675px; margin: auto;">
+      <ProjectReel />
     </div>
-
   </main>
 </template>
---
+++ src/components/ProjectReel.vue
@@ -0,0 +1,39 @@
+<script setup>
+//defineProps({
+//  msg: {
+//    type: String,
+//    required: true
+//  }
+//})
+</script>
+
+<template>
+      <div>
+        <div>
+          <v-carousel cycle="true" height="500px"> <!-- height is on here -->
+            <v-carousel-item
+              src="https://fastly.picsum.photos/id/907/500/500.jpg?hmac=1JG7-2YxQq4FkxYTIwNTsXH1FJw5WZoGoXCTCONnoJw"
+              cover
+            ></v-carousel-item>
+
+            <v-carousel-item
+              src="https://fastly.picsum.photos/id/128/1080/566.jpg?hmac=WdBcuFwIPlWtFn1Xh5KfObTiai0-vfZB7CWDU__8m2o"
+              cover
+            ></v-carousel-item>
+
+            <v-carousel-item
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              cover
+            ></v-carousel-item>
+          </v-carousel>
+        </div>
+
+        <div>
+          <div> <b> JAVA, Spring boot, APIs </b> </div>
+          <div> Created a weather forecasting app in Java that integrates with public APIs to display real-time weather data and seven-day forecasts, featuring a clean, intuitive GUI built with JavaFX. </div>
+        </div>
+      </div>
+</template>
+
+<style scoped>
+</style>
