6c67c3ece6aef2ee2fe8f2b98ac40fb85dba2114
Reece <reece.cares@gmail.com>
2024-11-01 18:27:25 +1100

    Add a configured overlay component

---
+++ src/components/Overlay.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-overlay v-model="overlay" content-class="overlay">
+      <template #activator="{ isActive, props }">
+        <v-btn v-bind="props">
+          Overlay is {{ isActive ? 'open' : 'closed' }}
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title class="text-h5">Overlay Content</v-card-title>
+        <v-card-text>This is the content inside the overlay.</v-card-text>
+      </v-card>
+    </v-overlay>
+  </div>
+</template>
+
+
+<style>
+.overlay
+{
+  margin: auto;
+  position: relative;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      overlay: true
+    };
+  }
+};
+</script>
